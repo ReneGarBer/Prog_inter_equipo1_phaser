464a4a76358e3b079af1e531fbc9b070a68591c5
ReneGarBer <67211759+ReneGarBer@users.noreply.github.com>
2022-02-26 12:56:12 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head> 
     <meta charset="UTF-8" />
     <title>Making your first Phaser 3 Game - Part 10</title>
+    //referencia a phaser
     <script src="//cdn.jsdelivr.net/npm/phaser@3.11.0/dist/phaser.js"></script>
     <style type="text/css">
         body {
@@ -11,6 +12,7 @@
     </style>
 </head>
 <body>
+    //referencia a index.js
     <script src="js/index.js" type="text/javascript"></script>
 </body>
 </html>
